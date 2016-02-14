fd6d840e7f89a188aa3f9ee279c299737c881d96
kirschbombe <kirschbombe@gmail.com>
2016-02-14 14:12:30 -0800

    initial commit

---
+++ Analyzing_the_Content_files/Analyzing_the_Content.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20120809
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Analyzing_the_Content_files/Analyzing_the_ContentMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ Critically_Appraising_a_Source_files/Critically_Appraising_a_Source.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20120809
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Critically_Appraising_a_Source_files/Critically_Appraising_a_SourceMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
